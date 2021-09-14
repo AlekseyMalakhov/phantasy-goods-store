9ad5b17fd0fbce666df351ef5392cedc46ea68b5
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-09-14 17:48:55 +0300

    add user item in sidebar

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import AppHeader from "./app/components/AppHeader";
 import Screen from "./app/components/Screen";
-import AppCard from "./app/components/AppCard";
-import CardsList from "./app/screens/CardsList";
-import ItemScreen from "./app/screens/ItemScreen";
-import items from "./app/config/items";
-import { Button } from "react-native-elements";
-import Icon from "react-native-vector-icons/MaterialIcons";
-import colors from "./app/config/colors";
-import BackButton from "./app/components/BackButton";
-import BuyScreen from "./app/screens/BuyScreen";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import CreateItemScreen from "./app/screens/CreateItemScreen";
-import UserSidebar from "./app/components/UserSidebar";
-import AppNavigator from "./navigation/AppNavigator";
-import AuthNavigator from "./navigation/AuthNavigator";
+import Sidebar from "./app/components/Sidebar";
 
 const styles = StyleSheet.create({
     // container: {
@@ -30,26 +13,11 @@ const styles = StyleSheet.create({
     // },
 });
 
-const Drawer = createDrawerNavigator();
-
 export default function App() {
     return (
         <Screen>
             <NavigationContainer>
-                <Drawer.Navigator
-                    initialRouteName="MainScreen"
-                    screenOptions={{
-                        headerShown: true,
-                        header: ({ navigation, route, options }) => <AppHeader navigation={navigation} />,
-                        drawerStyle: {
-                            marginTop: 58,
-                        },
-                    }}
-                >
-                    <Drawer.Screen name="Items" component={AppNavigator} />
-                    <Drawer.Screen name="Auth" component={AuthNavigator} />
-                    <Drawer.Screen name="CreateItemScreen" component={CreateItemScreen} />
-                </Drawer.Navigator>
+                <Sidebar />
             </NavigationContainer>
         </Screen>
     );
---
+++ app/components/Sidebar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import AppHeader from "./AppHeader";
+import AppNavigator from "../../navigation/AppNavigator";
+import AuthNavigator from "../../navigation/AuthNavigator";
+import CreateItemScreen from "../../app/screens/CreateItemScreen";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import colors from "../config/colors";
+
+const styles = StyleSheet.create({
+    container: {},
+});
+
+const Drawer = createDrawerNavigator();
+
+function Sidebar(props) {
+    return (
+        <Drawer.Navigator
+            initialRouteName="MainScreen"
+            screenOptions={{
+                headerShown: true,
+                header: ({ navigation, route, options }) => <AppHeader navigation={navigation} />,
+                drawerStyle: {
+                    marginTop: 58,
+                },
+            }}
+        >
+            <Drawer.Screen
+                name="User"
+                component={AppNavigator}
+                options={{
+                    drawerIcon: ({ focused }) => (
+                        <Icon color={colors.primaryColor} size={35} name={focused ? "account-circle" : "account-circle-outline"} />
+                    ),
+                }}
+            />
+            <Drawer.Screen name="Items" component={AppNavigator} />
+            <Drawer.Screen name="Auth" component={AuthNavigator} />
+            <Drawer.Screen name="CreateItemScreen" component={CreateItemScreen} />
+        </Drawer.Navigator>
+    );
+}
+
+export default Sidebar;
