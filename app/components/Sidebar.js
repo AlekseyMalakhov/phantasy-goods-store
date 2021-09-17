a87d49d750ec4337f1b799fcf2cae61ac996c6b9
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-09-17 18:23:50 +0300

    handle navigation after login

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -20,7 +20,7 @@ function Sidebar(props) {
     const user = useSelector((state) => state.user.user);
     return (
         <Drawer.Navigator
-            initialRouteName="MainScreen"
+            initialRouteName="Items"
             screenOptions={{
                 headerShown: true,
                 header: ({ navigation, route, options }) => <AppHeader navigation={navigation} />,
@@ -31,16 +31,15 @@ function Sidebar(props) {
         >
             <Drawer.Screen
                 name="User"
-                component={UserScreen}
+                component={user.name ? UserScreen : AuthNavigator}
                 options={{
-                    title: user ? user.name : "User",
+                    title: user.name ? user.name : "Anonymous user",
                     drawerIcon: ({ focused }) => (
                         <Icon color={colors.primaryColor} size={35} name={focused ? "account-circle" : "account-circle-outline"} />
                     ),
                 }}
             />
             <Drawer.Screen name="Items" component={AppNavigator} />
-            <Drawer.Screen name="Auth" component={AuthNavigator} />
             <Drawer.Screen name="CreateItemScreen" component={CreateItemScreen} />
         </Drawer.Navigator>
     );
