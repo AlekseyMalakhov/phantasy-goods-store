7111039c5e32b8c52252f0afa916cb118b67e4f2
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-09-22 18:14:09 +0300

    make all cart touchable

--- app/components/CartHeaderButton.js
+++ app/components/CartHeaderButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { Icon } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 import colors from "../config/colors";
@@ -27,14 +27,16 @@ function CartHeaderButton(props) {
     const navigation = useNavigation();
     const cart = useSelector((state) => state.user.cart);
     return (
-        <View style={styles.container}>
-            <Icon color="#fff" type="material-community" name="cart-outline" onPress={() => navigation.navigate("BuyScreen")} />
-            {cart.length > 0 ? (
-                <View style={styles.dot}>
-                    <Text style={styles.number}>{cart.length}</Text>
-                </View>
-            ) : null}
-        </View>
+        <TouchableOpacity onPress={() => navigation.navigate("BuyScreen")}>
+            <View style={styles.container}>
+                <Icon color="#fff" type="material-community" name="cart-outline" />
+                {cart.length > 0 ? (
+                    <View style={styles.dot}>
+                        <Text style={styles.number}>{cart.length}</Text>
+                    </View>
+                ) : null}
+            </View>
+        </TouchableOpacity>
     );
 }
 
