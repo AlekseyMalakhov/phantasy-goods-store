4e6098a81e70d7739326475186d1580733eaa104
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-09-27 18:14:42 +0300

    write messages component update

--- app/screens/WriteMessageScreen.js
+++ app/screens/WriteMessageScreen.js
@@ -2,13 +2,21 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
 });
 
-function WriteMessageScreen(props) {
+function WriteMessageScreen({ route }) {
+    const { item } = route.params;
     return (
         <View style={styles.container}>
-            <Text>Send message to seller</Text>
+            <Text style={styles.title}>Send message to {item.seller.name}</Text>
         </View>
     );
 }
